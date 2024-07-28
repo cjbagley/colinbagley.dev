1483f8321bc1a0c92cc371661acd7b0c6887f76f
Colin Bagley <cjbagley@googlemail.com>
2024-07-28 16:35:30 +0100

    feat: Added article template partial

--- internal/server/server.go
+++ internal/server/server.go
@@ -69,7 +69,7 @@ func HandleIndex(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.Header().Set("Access-Control-Allow-Methods", "GET")
 	w.WriteHeader(200)
-	tpl := template.Must(template.ParseFiles(getLayoutDirPath("main.gohtml"), getPageDirPath("index.gohtml")))
+	tpl := template.Must(template.ParseFiles(getLayoutDirPath("main.gohtml"), getPageDirPath("index.gohtml"), getLayoutDirPath("partials/article.gohtml")))
 	tpl.Execute(w, nil)
 }
 
