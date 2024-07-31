e482b8b06a9a2eb25f6a9a312ddc85fe4cafcd9c
Colin Bagley <cjbagley@googlemail.com>
2024-07-31 19:09:21 +0100

    feat: Added Published template for displaying formatted date

--- internal/server/templates.go
+++ internal/server/templates.go
@@ -10,8 +10,13 @@ import (
 )
 
 type PageData struct {
-	Title    string
-	Articles []data.Article
+	Title     string
+	Published string
+	Articles  []data.Article
+}
+
+func PrintDate(date string) string {
+	return date
 }
 
 type pageTemplates interface {
@@ -45,15 +50,26 @@ func (a *articlePageTemplates) getTemplates() []string {
 		getLayoutDirPath("main.gohtml"),
 		getPageDirPath("articles/" + a.article.Template + ".gohtml"),
 		getLayoutDirPath("partials/article.gohtml"),
+		getLayoutDirPath("partials/published.gohtml"),
 	}
 }
 
 func (a *articlePageTemplates) getData() PageData {
-	return PageData{Title: a.article.Title}
+	return PageData{Title: a.article.Title, Published: a.article.Published}
 }
 
 func WriteHttpResponse(w http.ResponseWriter, templates pageTemplates) error {
-	tpl := template.Must(template.ParseFiles(templates.getTemplates()...))
+	funcs := template.FuncMap{
+		"date": PrintDate,
+	}
+
+	tpl, err := template.New("main.gohtml").Funcs(funcs).ParseFiles(templates.getTemplates()...)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
 	tpl.Execute(w, templates.getData())
 	w.Header().Set("Content-Type", "text/html")
 
